f5cd694f1d96167af7480da0c2b4a93dee3a6670
jnabnet <jnabnet@gmail.com>
2016-08-16 06:19:18 -0700

    Basic work arounds

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Mercnet Theme',
+    'summary': 'Mercnet Theme',
+    'description': 'Website theme to showcase customization possibilities.',
+    'category': 'Theme',
+    'version': '1.0.1',
+    'author': 'Jnabnet',
+    'depends': ['website','website_crm'],
+    'images': ['images/mercury_homepage.jpeg'],
+    'data': [
+        'views/theme_source.xml',
+        'views/snippets.xml',
+    ],
+    'installable': True,
+    'application':True,
+}
